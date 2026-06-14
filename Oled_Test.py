6fc6c439d16dbebe074180f9aacc347de053e8af
DuquetteLiam <liam.duquette@gmail.com>
2026-06-14 13:33:03 -0400

    Added Oled testing program

---
+++ Oled_Test.py
@@ -0,0 +1,18 @@
+# Simple program to get the OLED Screen working on the pi
+
+from board import SCL, SDA
+
+import adafruit_ssd1306
+import busio
+
+i2c = busio.I2C(SCL, SDA)
+
+display = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
+
+display.fill(1)
+
+display.show
+
+sleep(1000)
+
+display.fill(0)
