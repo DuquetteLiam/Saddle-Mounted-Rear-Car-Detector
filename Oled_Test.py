b10f97e8cca61ea07b8735a6855a7a42acbaac17
DuquetteLiam <liam.duquette@gmail.com>
2026-06-14 13:57:29 -0400

    adding rectangle

--- Oled_Test.py
+++ Oled_Test.py
@@ -4,6 +4,7 @@ from board import SCL, SDA
 import time
 from luma.core.interface.serial import i2c
 from luma.oled.device import sh1106 
+from PIL import Image, ImageDraw, ImageFont
 
 serial = i2c(port = 1, address = 0x3C)
 
@@ -11,6 +12,10 @@ device = sh1106(serial, width = 128, height = 64)
 
 device.clear()
 
-draw.rectangle((0, 0, 127, 63), outline=255, fill=0)
+image = Image.new("1", (128, 64))
+draw = ImageDraw.Draw(image)
+
+draw.rectangle((0, 0, 127, 63), outline=255, fill=1)
+device.display(image)
 
 time.sleep(5)
