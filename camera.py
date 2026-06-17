e8e4df94894e10211dc9142204b5d86ff2f89927
DuquetteLiam <liam.duquette@gmail.com>
2026-06-17 15:24:22 -0400

    learning how to get ai data from camera

--- camera.py
+++ camera.py
@@ -1,3 +1,5 @@
+import time
+
 from picamera2 import Picamera2
 from picamera2.devices import IMX500
 
@@ -8,3 +10,8 @@ config = picam2.create_video_configuration()
 picam2.start(config)
 metadata = picam2.capture_metadata()
 network_outputs = imx500.get_outputs(metadata)
+
+time.sleep(5)
+
+while True:
+    print(network_outputs)
